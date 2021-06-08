21584f0bcf2ca2fc0735f9c6e5db8575e9ade4a1
favonius-weiss <85563817+favonius-weiss@users.noreply.github.com>
2021-06-09 03:15:43 +1000

    wish calculator

---
+++ Assets/wishcalc.js
@@ -0,0 +1,57 @@
+window.onload = function currentDate() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = yyyy + "-" + mm + "-" + dd;
+  document.getElementById("startDate").value = today;
+  document.getElementById("endDate").value = today;
+}
+
+  function calculate() {
+  var x = document.getElementById("frm1");
+  var totalWishes = parseInt("");
+  var primos = parseInt(document.getElementById("primos").value);
+  var fates = parseInt(document.getElementById("fates").value);
+  var welkin = parseInt(document.getElementById("welkin").value);
+  var gnostic = parseInt(document.getElementById("gnostic").value);
+  var genesis = parseInt(document.getElementById("genesis").value);
+
+  var startDate = document.getElementById("startDate").value;
+  var endDate = document.getElementById("endDate").value;
+  if (!endDate) {
+    daysUntilEnd = parseInt(0);
+  } else {
+    var diffInMs = new Date(endDate) - new Date(startDate);
+    var daysUntilEnd = parseInt(diffInMs / (1000 * 60 * 60 * 24));
+  }
+
+  var primosLeft = parseInt("0");
+
+  var pities = parseInt("0");
+  var wishType = document.getElementById("wishType").value;
+
+  if (welkin > daysUntilEnd) {
+    welkin = daysUntilEnd;
+  }
+
+  primos += (daysUntilEnd * 60) + (welkin * 90) + (gnostic * 680) + genesis;
+  fates += (gnostic * 4);
+
+  primosLeft = (primos % 160);
+  totalWishes = Math.floor(primos / 160) + fates;
+
+  if (wishType == "character") {
+    pities = totalWishes / 90;
+  } else if (wishType == "weapon") {
+    pities = totalWishes / 80;
+  }
+
+
+  document.getElementById("displayDaysUntilEnd").innerHTML = "Days before banner ends: " + daysUntilEnd;
+
+  document.getElementById("displayWishes").innerHTML = "Total Wishes: " + totalWishes;
+  document.getElementById("displayRemPrimos").innerHTML = "Remaining Primogems: " + primosLeft;
+  document.getElementById("displayPity").innerHTML = "Number of hard pity: " + pities;
+}
